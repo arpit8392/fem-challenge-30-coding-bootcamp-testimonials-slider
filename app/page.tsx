9f7df8d12b986885cde8ec51e532a4f7b4962544
Arpit Namdev <arpit8392@gmail.com>
2023-06-22 18:24:02 +0530

    Add overlay image over text

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 			<div className='fixed inset-0 -z-50 bg-patternCurve bg-contain bg-left-bottom bg-no-repeat md:bg-auto' />
 			<section className='md:px-40 md:py-12'>
 				<div className='flex flex-col items-center justify-center gap-20 md:flex-row md:gap-0'>
-					<div className='relative md:order-1'>
+					<div className='relative md:order-1 md:translate-x-1/2'>
 						<div className='absolute inset-0 bg-patternBg bg-contain bg-no-repeat' />
 						<div className='relative m-8 md:h-64 md:w-64 lg:h-[480px] lg:w-[480px]'>
 							<Image
@@ -43,7 +43,7 @@ export default function Home() {
 						</div>
 					</div>
 
-					<div className='relative z-50'>
+					<div className='relative z-50 md:absolute md:left-0 md:px-40' >
 						<div className='absolute inset-0 -z-50 -translate-y-1/3 translate-x-1/3 bg-patternQuotes bg-no-repeat md:-translate-y-1/4 md:translate-x-1/4' />
 						<Testimonial {...testimonialsData[index]} />
 					</div>
