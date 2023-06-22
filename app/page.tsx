81cfee8cf7fd11338aae55edafb9711d1d4f7b41
Arpit Namdev <arpit8392@gmail.com>
2023-06-22 18:36:35 +0530

    Bug Fix: Image Layout Issue on Medium Screens

--- app/page.tsx
+++ app/page.tsx
@@ -22,13 +22,13 @@ export default function Home() {
 	}
 
 	return (
-		<main className='grid min-h-screen place-content-center overflow-hidden '>
+		<main className='grid min-h-screen place-content-center overflow-x-hidden '>
 			<div className='fixed inset-0 -z-50 bg-patternCurve bg-contain bg-left-bottom bg-no-repeat md:bg-auto' />
 			<section className='md:px-40 md:py-12'>
 				<div className='flex flex-col items-center justify-center gap-20 md:flex-row md:gap-0'>
 					<div className='relative md:order-1 md:translate-x-1/2'>
 						<div className='absolute inset-0 bg-patternBg bg-contain bg-no-repeat' />
-						<div className='relative m-8 md:h-64 md:w-64 lg:h-[480px] lg:w-[480px]'>
+						<div className='relative m-8 md:h-[540px] md:w-[540px]'>
 							<Image
 								src={`/images/${testimonialsData[index].image}`}
 								alt={testimonialsData[index].author}
@@ -43,7 +43,7 @@ export default function Home() {
 						</div>
 					</div>
 
-					<div className='relative z-50 md:absolute md:left-0 md:px-40' >
+					<div className='relative z-50 md:absolute md:left-0 md:px-40'>
 						<div className='absolute inset-0 -z-50 -translate-y-1/3 translate-x-1/3 bg-patternQuotes bg-no-repeat md:-translate-y-1/4 md:translate-x-1/4' />
 						<Testimonial {...testimonialsData[index]} />
 					</div>
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,12 +1,12 @@
 const Testimonial = ({ testimony, author, position }: Testimony) => {
 	return (
-		<div className='flex max-w-sm flex-col gap-5 text-center md:max-w-2xl md:text-left	'>
-			<blockquote className='text-lg font-extralight text-darkBlue md:text-[32px]/[44px]'>
+		<div className='flex max-w-sm flex-col gap-5 text-center md:text-left lg:max-w-2xl	'>
+			<blockquote className='text-lg font-extralight text-indigo-950 md:text-[32px]/[44px]'>
 				{`“ ${testimony} ”`}
 			</blockquote>
 			<div className='flex flex-col items-center md:flex-row md:gap-2'>
-				<h1 className='font-bold text-darkBlue md:text-xl'>{author}</h1>
-				<h2 className='font-medium text-grayishBlue md:text-xl'>{position}</h2>
+				<h1 className='font-bold text-indigo-950 md:text-xl'>{author}</h1>
+				<h2 className='font-medium text-slate-300 md:text-xl'>{position}</h2>
 			</div>
 		</div>
 	)
